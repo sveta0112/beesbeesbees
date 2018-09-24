764378fef713bda1c49db79a445bc93cde34cdcf
sveta0112 <sveta0112@hotmail.com>
2018-09-24 12:31:43 -0700

    Added implementation to forageBee.js

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,17 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
+ForagerBee.prototype = Object.create(HoneyMakerBee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+ForagerBee.prototype.forage = function(foundTreasure){
+  if(this.age === 40){
+  	return 'I am too old, let me play cards instead';
+  }	
+  this.treasureChest.push(foundTreasure);
+};
+
 
