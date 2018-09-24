309278573d416685c32aa33e20cab7043fe6083e
sveta0112 <sveta0112@hotmail.com>
2018-09-24 12:32:37 -0700

    Added implememntation to retiredForager.js

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,13 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+};
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;	
+RetiredForagerBee.prototype.gamble = function(treasure){
+  this.treasureChest.push(treasure);
 };
 
