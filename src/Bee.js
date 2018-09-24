f723223e1f2bdb41d1bbe28eaf50a27abe928965
sveta0112 <sveta0112@hotmail.com>
2018-09-24 12:29:37 -0700

    Added implementation to bee.js

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
 var Bee = function() {
+  Grub.call(this);//this--> Bee
+  //before: {age: 0, color: "pink", food: "jelly"}
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
+  //after:{age: 5, color: "yellow", food: "jelly", job: "keep on growing"}
 };
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+
 
